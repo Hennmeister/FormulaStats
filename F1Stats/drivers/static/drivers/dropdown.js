909b31f6b5694ce44fc400fbac7462bd6623acb7
Hennmeister <henning.lindig@rogers.com>
2019-12-23 10:24:03 -0500

    testing searchbar changes

--- F1Stats/drivers/static/drivers/dropdown.js
+++ F1Stats/drivers/static/drivers/dropdown.js
@@ -1,12 +1,22 @@
+toChange = false;
+
 function showDrivers() {
-  document.getElementById("myDropdown").classList.toggle("show");
+    dropdown = document.getElementById("dropdown")
+    if (!dropdown.classList.contains("show")) {
+        dropdown.classList.add("show")
+        document.getElementById("searchBox").focus();
+        toChange = false;
+    }
+    else {
+        toChange = true;
+    }
 }
 
 function filterFunction() {
   var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
+  input = document.getElementById("searchBox");
   filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
+  div = document.getElementById("dropdown");
   a = div.getElementsByTagName("a");
   for (i = 0; i < a.length; i++) {
     txtValue = a[i].textContent || a[i].innerText;
@@ -16,4 +26,11 @@ function filterFunction() {
       a[i].style.display = "none";
     }
   }
+}
+
+function deselect() {
+    if (toChange && document.getElementById("dropdown").classList.contains("show")) {
+        document.getElementById("dropdown").classList.remove("show");
+    }
+    toChange = !toChange;
 }
