d8f3853847c3872aa8e247531687e89196f7327c
Hennmeister <henning.lindig@rogers.com>
2019-12-28 12:53:21 -0500

    Updated search div opening and closing

--- F1Stats/drivers/static/drivers/dropdown.js
+++ F1Stats/drivers/static/drivers/dropdown.js
@@ -1,10 +1,23 @@
+let open = false;
+
 function showDrivers() {
-  document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown").classList.add("show");
+    open = true;
+}
+
+function closeSearch() {
+    console.log(open);
+    document.getElementById("myDropdown").classList.remove("show");
+    if (open){
+        document.getElementById("myDropdown").classList.add("show");
+        open = false;
+    }
+    open = false;
 }
 
 function filterFunction() {
   var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
+  input = document.getElementById("searchBox");
   filter = input.value.toUpperCase();
   div = document.getElementById("myDropdown");
   a = div.getElementsByTagName("a");
