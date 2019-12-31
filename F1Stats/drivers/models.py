830074f4a12033f0a40092650a35e09048b9861c
Hennmeister <henning.lindig@rogers.com>
2019-12-31 14:02:41 -0500

    Added driver data to database

--- F1Stats/drivers/models.py
+++ F1Stats/drivers/models.py
@@ -13,6 +13,24 @@ class Driver(models.Model):
 	last_name = models.CharField(max_length=30)
 	constructor = models.ForeignKey(Constructor, on_delete=models.CASCADE)
 
+	career_wins = models.IntegerField(default=0)
+	career_podiums = models.IntegerField(default=0)
+	career_poles = models.IntegerField(default=0)
+	career_points = models.IntegerField(default=0)
+	career_seasons = models.IntegerField(default=0)
+	career_races = models.IntegerField(default=0)
+
+	season_points = models.IntegerField(default=0)
+	season_position = models.IntegerField(default=0)
+	season_wins = models.IntegerField(default=0)
+	season_podiums = models.IntegerField(default=0)
+	season_avg_overtakes = models.FloatField(default=0)
+	season_beat_teammate_rate = models.IntegerField(default=0)
+	season_DNF_rate = models.IntegerField(default=0)
+
+
+
+
 	def __str__(self):
 		return self.last_name
 
